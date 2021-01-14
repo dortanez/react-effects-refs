7fc5a6a942a0f7febce6632f2d6aac14bd532798
Danisa Ortanez <dortanez@yahoo.com>
2021-01-13 23:19:32 -0800

    effects refs

--- src/DeckOfCards.js
+++ src/DeckOfCards.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
 
 const DeckOfCards = () => {
+    const [deck, setDeck] = useState(null);
+    useEffect(() => {
+        async function getCard() {
+            const res = await axios.get('https://deckofcardsapi.com/api/deck/')
+            setDeck(res)
+        }
+        getCard();
+
+    },[setDeck])
+    
+
+
     return (
         <div>
-            
+            <button>GIMME A CARD!</button>
         </div>
 
     )
