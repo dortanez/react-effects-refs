324f3243cb84c5cba1bf733c9a0225dac175d04b
Danisa Ortanez <dortanez@yahoo.com>
2021-01-14 21:40:55 -0800

    deck of cards

--- src/DeckOfCards.js
+++ src/DeckOfCards.js
@@ -1,22 +1,33 @@
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
+import './DeckOfCards.css';
 
 const DeckOfCards = () => {
     const [deck, setDeck] = useState(null);
+    const [drawnCard, setDrawnCard] = useState(null);
     useEffect(() => {
+        async function newDeck() {
+            const res = await axios.get('https://deckofcardsapi.com/api/deck/new');
+            await axios.get(`https://deckofcardsapi.com/api/deck/${res.data.deck_id}/shuffle/`);
+            setDeck(res.data.deck_id)
+        }
+        newDeck();
+    },[setDeck])
+
+    const drawCard = () => {
         async function getCard() {
-            const res = await axios.get('https://deckofcardsapi.com/api/deck/')
-            setDeck(res)
+            const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`);
+            setDrawnCard(res.data.cards[0].image);
+            console.log(res)
+
         }
         getCard();
-
-    },[setDeck])
-    
-
+    }
 
     return (
         <div>
-            <button>GIMME A CARD!</button>
+            <button onClick={drawCard}>GIMME A CARD!</button>
+            <img src={drawnCard}/>
         </div>
 
     )
